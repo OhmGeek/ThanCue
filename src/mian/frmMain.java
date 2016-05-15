5d1aa1376a89989fac0bf08a39d5078c4ecd3257
mikecroall <mikebcroall@gmail.com>
2016-05-15 20:48:37 +0100

    Java migration

---
+++ src/mian/frmMain.java
@@ -0,0 +1,12 @@
+package mian;
+
+import javax.swing.*;
+
+/**
+ * Created by mike on 15/05/16.
+ */
+public class frmMain {
+    private JButton btnNextCue;
+    private JPanel panel1;
+    private JList list1;
+}
