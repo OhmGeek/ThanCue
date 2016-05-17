84bf21a2996e82d3ad614165374eb076fad520d9
mikecroall <mikebcroall@gmail.com>
2016-05-17 19:49:09 +0100

    Menu! Mike made menu! Yas!

--- src/mian/Main.java
+++ src/mian/Main.java
@@ -6,16 +6,13 @@ import javax.swing.*;
  * Created by ryan on 15/05/16.
  */
 
-
-
-
 public class Main {
     public static void main(String[] args) {
         JFrame window = new JFrame("ThanCue");
-        frmMain mainPanel = new frmMain();
+        frmMain mainPanel = new frmMain(window);
 
         window.setContentPane(mainPanel.getPanel());
-        window.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.pack();
         window.setVisible(true);
     }
--- src/mian/frmMain.java
+++ src/mian/frmMain.java
@@ -1,6 +1,7 @@
 package mian;
 
 import javax.swing.*;
+import java.awt.event.KeyEvent;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -12,47 +13,91 @@ import java.util.Random;
  */
 public class frmMain {
 
+    private JFrame frame;
     private JPanel pnlMain;
     private JList lstCues;
-    private JButton btnNextCue;
-    private JButton btnAddCue;
-    private JButton btnMoveUp;
-    private JButton btnMoveDown;
+    private JButton btnNextCue, btnAddCue, btnMoveUp, btnMoveDown;
+    private JMenuBar menuBar;
+    private JMenu menuFile, menuEdit;
+    private JMenuItem menuItemFileNew, menuItemFileOpen, menuItemFileSave, menuItemFileSaveAs;
+    private JCheckBoxMenuItem menuItemShowMode;
+
     private List<Cue> cueCollection;
     private Random r; // purely for adding test cues todo remove
 
-
-
-
-    public frmMain() {
-
-        new FileDrop(this.getPanel(), files -> {
-            if(cueCollection != null) {
-
-                for (File f : files)
-                {
-                    //todo add checking to the type. Only allow for sound cues or videos.
-                     SoundCue cToAdd = new SoundCue();
-                     cToAdd.setCueName("New Dragged Cue" + " " + f.getAbsolutePath());
-                     cToAdd.setFilePath(f.getAbsolutePath());
-                     cueCollection.add(cToAdd);
-                     lstCues.setListData(cues());
-                }
-            }
-        });
-
-
-
+    public frmMain(JFrame frame) {
+        this.frame = frame;
 
         r = new Random();
 
-        cueCollection = new ArrayList<>(); //this stores the latest cues. We can load this in from somewhere or create
-        //new.
-        populateCueCollection(); //this is a test.
-
+        cueCollection = new ArrayList<>();
+        populateCueCollection();
         lstCues.setListData(cues());
 
         registerActionListeners();
+        createMenu();
+        registerFileDrop();
+    }
+
+    private void registerFileDrop() {
+        new FileDrop(this.getPanel(), files -> {
+            if (cueCollection != null) {
+                for (File f : files) {
+                    //todo add checking to the type. Only allow for sound cues or videos.
+                    SoundCue cToAdd = new SoundCue();
+                    cToAdd.setCueName("New Dragged Cue - " + f.getAbsolutePath());
+                    cToAdd.setFilePath(f.getAbsolutePath());
+                    cueCollection.add(cToAdd);
+                    lstCues.setListData(cues());
+                }
+            }
+        });
+    }
+
+    private void createMenu() {
+        menuBar = new JMenuBar();
+
+        menuFile = new JMenu("File");
+        menuFile.setMnemonic(KeyEvent.VK_F);
+        menuEdit = new JMenu("Edit");
+        menuEdit.setMnemonic(KeyEvent.VK_E);
+
+        menuItemFileNew = new JMenuItem("New", new ImageIcon());
+        menuItemFileNew.setMnemonic(KeyEvent.VK_N);
+        menuItemFileNew.addActionListener(actionEvent -> {
+            JOptionPane.showMessageDialog(null, "New Cue dialog here", "Test", JOptionPane.INFORMATION_MESSAGE);
+        });
+        menuItemFileOpen = new JMenuItem("Open");
+        menuItemFileOpen.setMnemonic(KeyEvent.VK_O);
+        // todo action listeners from here down
+        menuItemFileSave = new JMenuItem("Save");
+        menuItemFileSave.setMnemonic(KeyEvent.VK_S);
+        menuItemFileSaveAs = new JMenuItem("Save as");
+        // todo what mnemonic for save as?
+        menuItemShowMode = new JCheckBoxMenuItem("Show mode");
+        menuItemShowMode.addActionListener(actionEvent -> {
+            if (menuItemShowMode.isSelected()) {
+                btnAddCue.setEnabled(false);
+                btnMoveUp.setEnabled(false);
+                btnMoveDown.setEnabled(false);
+            } else {
+                btnAddCue.setEnabled(true);
+                btnMoveUp.setEnabled(true);
+                btnMoveDown.setEnabled(true);
+            }
+        });
+
+        menuBar.add(menuFile);
+        menuFile.add(menuItemFileNew);
+        menuFile.add(menuItemFileOpen);
+        menuFile.add(menuItemFileSave);
+        menuFile.add(menuItemFileSaveAs);
+        menuFile.addSeparator();
+        menuFile.add(menuItemShowMode);
+
+        menuBar.add(menuEdit);
+
+        frame.setJMenuBar(menuBar);
     }
 
     private void registerActionListeners() {
