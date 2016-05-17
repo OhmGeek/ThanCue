1dae9c8d4c32f3518409df16e21e3f810f51e01a
OhmGeek <ryan@ohmgeek.co.uk>
2016-05-17 15:05:23 +0100

    Created a Program Manager class ProgManager. This allows for basic GUI controls to be implemented, such as changing the view, setting the window title, and stuff like that.

---
+++ src/mian/ProgManager.java
@@ -0,0 +1,32 @@
+package mian;
+
+import javax.swing.*;
+
+/**
+ * Created by ryan on 17/05/16.
+ */
+public class ProgManager {
+    private JFrame window;
+    private static final String PROG_NAME = "ThanCue";
+
+    
+    public ProgManager() {
+        window = new JFrame();
+        setWindowTitle(PROG_NAME);
+        PanelMain panel = new PanelMain();
+        setContentDisplay(panel);
+    }
+
+    private void setContentDisplay(JPanel p) {
+        window.setContentPane(p);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.pack();
+        window.setVisible(true);
+    }
+
+    private void setWindowTitle(String title) {
+        window.setTitle(title);
+    }
+
+
+}
