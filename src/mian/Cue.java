a91c6133a89cf9885a1dd7cd1f8aa2a37f257fcf
OhmGeek <ryan@ohmgeek.co.uk>
2016-05-17 15:35:11 +0100

    Dynamic cue loading

--- src/mian/Cue.java
+++ src/mian/Cue.java
@@ -5,6 +5,23 @@ package mian;
  */
 public abstract class Cue {
 
+    private String cueName;
+
+    public Cue() {
+        cueName = "Unnamed Cue";
+    }
+
+    public String getCueName() {
+        return cueName;
+    }
+    public void setCueName(String name) {
+        cueName = name;
+    }
+    @Override
+    public String toString() {
+        return getCueName();
+    }
     public abstract void playCue(); //this plays the cue, be it lighting, sound, video or pushing over a table.
+
 
 }
--- src/mian/PanelMain.java
+++ src/mian/PanelMain.java
@@ -1,6 +1,8 @@
 package mian;
 
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by mike on 15/05/16.
@@ -9,7 +11,26 @@ public class PanelMain {
     private JPanel pnlMain;
     private JList lstCues;
     private JButton btnNextCue;
+    private List<Cue> cueCollection;
+    public PanelMain() {
+        cueCollection = new ArrayList<Cue>(); //this stores the latest cues. We can load this in from somewhere or create
+                                            //new.
 
+        populateCueCollection(); //this is a test.
+
+
+
+        lstCues.setListData(cueCollection.toArray());
+    }
+
+    private void populateCueCollection() {
+        Cue c1 = new SoundCue();
+        Cue c2 = new SoundCue();
+        c1.setCueName("Cue 1");
+        c2.setCueName("Cue 2");
+        cueCollection.add(c1);
+        cueCollection.add(c2);
+    }
     public JPanel getPanel() {
         return this.pnlMain;
     }
