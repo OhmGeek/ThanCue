8bd6b031f501c970e01a435ae82f244dbe2b7857
mikecroall <mikebcroall@gmail.com>
2016-05-19 17:00:37 +0100

    Oli made me do it.

--- src/ThanCue/frmMain.java
+++ src/ThanCue/frmMain.java
@@ -33,8 +33,8 @@ public class frmMain {
         //set the UI to match the OS default feel
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception ex) {
-            ex.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
         //set the jframe
