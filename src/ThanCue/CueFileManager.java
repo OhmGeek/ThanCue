5f013841074173f0e6274e46abdf99059fca7a79
OhmGeek <ryan@ohmgeek.co.uk>
2016-05-20 12:00:41 +0100

    forgot to add files last time.. oops!

--- src/ThanCue/CueFileManager.java
+++ src/ThanCue/CueFileManager.java
@@ -1,7 +1,17 @@
 package ThanCue;
 
+import java.util.List;
+
 /**
  * Created by ryan on 18/05/16.
  */
 public class CueFileManager {
+    public CueFileManager() {
+
+    }
+
+    public void writeCue(String filePath, List<Cue> cueCollection) throws Exception{
+
+    }
+
 }
