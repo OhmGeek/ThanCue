f1ebd9aace8b30151f8b494d159c982350862505
mikecroall <mikebcroall@gmail.com>
2016-05-23 19:30:27 +0100

    Hopefully updating working...

--- src/ThanCue/Main.java
+++ src/ThanCue/Main.java
@@ -1,6 +1,11 @@
 package ThanCue;
 
+import com.briksoftware.updatefx.core.UpdateFX;
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+
 import javax.swing.*;
+import java.io.IOException;
 
 /**
  * Created by ryan on 15/05/16.
@@ -8,6 +13,18 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
+        try {
+            new JFXPanel();
+            Platform.runLater(() -> {
+                // todo hmm... do I need to do something here?
+            });
+
+            UpdateFX updater = new UpdateFX(Main.class);
+            updater.checkUpdates();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
         JFrame window = new JFrame("ThanCue v0.5");
         frmMain mainPanel = new frmMain(window);
 
