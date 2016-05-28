b4f4b618d678fa117e63cbc5be63ad6239227ac7
OhmGeek <ryan@ohmgeek.co.uk>
2016-05-28 16:49:02 +0100

    Started the GUI rewrite in JavaFX. Still not done

--- src/ThanCue/Main.java
+++ src/ThanCue/Main.java
@@ -13,7 +13,6 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-
         primaryStage.setTitle("ThanCue: " + Constants.VERSION_NAME);
         primaryStage.setScene(new Scene(root,300,275));
         primaryStage.show();
