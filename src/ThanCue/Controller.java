3290d8bc5d00d05bcf5f95815937a7ff7e3ac762
mikecroall <mikebcroall@gmail.com>
2016-05-28 14:27:42 +0100

    FX transition phase 1

---
+++ src/ThanCue/Controller.java
@@ -0,0 +1,4 @@
+package ThanCue;
+
+public class Controller {
+}
--- src/ThanCue/Main.java
+++ src/ThanCue/Main.java
@@ -1,49 +1,23 @@
 package ThanCue;
 
-import com.briksoftware.updatefx.core.UpdateFX;
-import com.briksoftware.updatefx.model.Application;
-import com.sun.javafx.tk.Toolkit;
-import javafx.application.Platform;
-import javafx.embed.swing.JFXPanel;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-import javax.swing.*;
-import java.io.IOException;
+public class Main extends Application {
 
-/**
- * Created by ryan on 15/05/16.
- */
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
 
-public class Main {
-    public static final String VERSION_NAME = "ScriptMade";
 
     public static void main(String[] args) {
-        try {
-            new JFXPanel();
-            Platform.runLater(() -> {
-                // todo hmm... do I need to do something here?
-            });
-
-            UpdateFX updater = new UpdateFX(Main.class);
-            updater.checkUpdates();
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-
-        System.out.println("Running on OS: " + System.getProperty("os.name").toLowerCase());
-
-        Platform.setImplicitExit(false);
-
-        Platform.runLater(() -> {
-            Stage window = new Stage();
-            frmMain mainForm = new frmMain();
-            Pane p = mainForm.load();
-            System.out.println("IVE DONE SOMETHING DADDY!");
-            window.setScene(new Scene(p,500,500));
-            window.showAndWait();
-        });
-
+        launch(args);
     }
 }
