7710778240bab5b1318e165d260da6cb54e43f21
mikecroall <mikebcroall@gmail.com>
2016-05-28 20:40:14 +0100

    More FX gui progress - autosizing - text on the go button - this FX stuff took longer than I care to admit

--- src/ThanCue/Constants.java
+++ src/ThanCue/Constants.java
@@ -4,5 +4,5 @@ package ThanCue;
  * Created by ryan on 28/05/16.
  */
 public class Constants {
-    public static final String VERSION_NAME = "0.6";
+    public static final String VERSION_NAME = "0.7.1";
 }
