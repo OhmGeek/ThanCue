ec4ab20620f6a417d2c9d2fc4f9eedc9b7cc84da
mikecroall <mikebcroall@gmail.com>
2016-06-02 17:31:00 +0100

    Update test

--- src/ThanCue/Constants.java
+++ src/ThanCue/Constants.java
@@ -4,6 +4,6 @@ package ThanCue;
  * Created by ryan on 28/05/16.
  */
 public class Constants {
-    public static final String VERSION_NAME = "0.7.4b";
-    public static final int RELEASE_ID = 16;
+    public static final String VERSION_NAME = "0.7.4c";
+    public static final int RELEASE_ID = 17;
 }
