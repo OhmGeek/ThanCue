1c3e53cabfa74f7204ecca8cbac4a2214bac207d
OhmGeek <ryan@ohmgeek.co.uk>
2016-06-02 19:10:00 +0100

    updated version number of program

--- src/ThanCue/Constants.java
+++ src/ThanCue/Constants.java
@@ -4,6 +4,6 @@ package ThanCue;
  * Created by ryan on 28/05/16.
  */
 public class Constants {
-    public static final String VERSION_NAME = "0.7.4c";
-    public static final int RELEASE_ID = 17;
+    public static final String VERSION_NAME = "0.7d";
+    public static final int RELEASE_ID = 0.7.2;
 }
