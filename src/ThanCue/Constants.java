30e228df966158ba07d0b55d10240b8abb05fabf
OhmGeek <ryan@ohmgeek.co.uk>
2016-06-06 22:22:52 +0100

    Fixed drag drop, still clueless about cell index being -1

--- src/ThanCue/Constants.java
+++ src/ThanCue/Constants.java
@@ -4,6 +4,6 @@ package ThanCue;
  * Created by ryan on 28/05/16.
  */
 public class Constants {
-    public static final String VERSION_NAME = "0.7f";
-    public static final int RELEASE_ID = 19;
+    public static final String VERSION_NAME = "0.7g";
+    public static final int RELEASE_ID = 20;
 }
